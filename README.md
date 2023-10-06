24b4dcc332c642d11202faf63c8c53875ae492ee
Samkit Shah <samkit.shah@samkits-mbp.home>
2023-10-06 12:42:47 -0400

    Quick implementation of the final product

---
+++ README.md
@@ -0,0 +1,9 @@
+# WebSockets With GoLang
+Implementing [this amazing blogpost](https://programmingpercy.tech/blog/mastering-websockets-with-go/) about mastering websockets with go.
+
+
+## How to run
+- Run the server with : `go run *.go`
+- Visit http://localhost:8080
+- Keep an eye on the browser console and the stdout
+
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	setupAPI()
+	log.Println("Hello, I am running")
+	// Serve on port :8080, fudge yeah hardcoded port
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// setupAPI will start all Routes and their Handlers
+func setupAPI() {
+	// Create a Manager instance used to handle WebSocket Connections
+	manager := NewManager()
+
+	// Serve the ./frontend directory at Route /
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serveWS)
+}
